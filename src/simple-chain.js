d51196336d254c7b519927d3bcb2f261b47799ea
Kristen <kr1s10.khud@gmail.com>
2022-04-30 04:55:15 +0300

    feat: add simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (this.chain[position - 1]) {
+      this.chain.splice(position - 1, 1);
+      return this;
+    } else {
+      this.chain = [];
+      throw new Error("You can\'t remove incorrect link!");
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let res = this.chain.slice();
+    this.chain = [];
+    return res.join('~~');
   }
 };
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -16,6 +16,22 @@ const { NotImplementedError } = require('../extensions/index.js');
 function transform(/* arr */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  // console.log(arr)
+  // if (Array.isArray(arr)) {
+  //   const res = [...arr];
+  //     res.forEach((v, i, a) => {
+  //     if (v === '--discard-next') i === a.length - 1 ? a.splice(i, 1) : a.splice(i, 2);
+  //     if (v === '--discard-prev') i === 0 ? a.splice(i, 1) : a.splice(i - 1, 2);
+  //     if (v === '--double-next') i === a.length - 1 ? a.splice(i, 1) : a.splice(i, 1, i + 1);
+  //     if (v === '--double-prev') i === 0 ? a.splice(i, 1) : a.splice(i, 1, i - 1);
+
+  //   });
+  //   console.log(res)
+  //   return res;
+    
+  // } else {
+  //   throw new Error("'arr' parameter must be an instance of the Array!");
+  // }
 }
 
 module.exports = {
